2b4b58aa94a5514ed676d9686056f07a2ef92d01
Jenprpa <jenruenkam@gmail.com>
2026-09-01 16:12:29 +0700

    chore(deploy): configure and publish project to Firebase Hosting at rspg-school-garden.web.app

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/RSPG-School-Garden/',
+  base: './',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1500
